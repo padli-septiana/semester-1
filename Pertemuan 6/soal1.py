22e01009919bfca30abf6451e6de66c486088fbe
Muhammad Padli Septiana <144655010+padli-septiana@users.noreply.github.com>
2023-10-19 17:10:18 +0700

    Pertemuan 6

---
+++ Pertemuan 6/soal1.py
@@ -0,0 +1,10 @@
+nilai = int(input("Masukkan nilai: "))
+
+if nilai >= 90:
+    print("Nilai anda A")
+elif nilai >= 80:
+    print("Nilai anda B")
+elif nilai >= 70:
+    print("Nilai anda C")
+else:
+    print("Nilai anda D")
